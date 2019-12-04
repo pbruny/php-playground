6cb5c1ab734c80b92f4607a3c80e9c6e112f5bfc
Paulo Lima <paulobruny@gmail.com>
2019-12-04 00:13:22 -0300

    Adiciona projeto de site institucional

---
+++ Site Institucional/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Welcome to BMD Company</title>
+</head>
+<body>
+  <header>
+    <nav>
+      <ul class="desktop">
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul> <!-- desktop -->
+      <ul class="mobile">
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul><!-- mobile -->
+    </nav>
+  </header><!-- header -->
+
+  <section class="banner">
+    <form action="">
+      <h1>What is your main e-mail?</h1>
+      <input type="email" name="email" required>
+      <input type="submit" value="Register" name="register">
+    </form>
+  </section><!-- banner --> 
+
+  <main class="description">
+    <div class="w50">
+      <h2>BMG Company</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium aut inventore officia consequatur deserunt, sit iusto adipisci reiciendis consequuntur mollitia voluptas laborum amet quae culpa iste tempora rerum et beatae.
+      Non unde eum odio saepe reprehenderit cumque quas corporis optio obcaecati incidunt. Explicabo quam dolor porro, dignissimos cum earum veritatis, ullam architecto expedita ratione eligendi? Perspiciatis ea ipsam dolorum sed!
+      Voluptas repudiandae facere saepe corrupti, dolorum dolores, quo nemo error vitae laudantium tempore totam eligendi necessitatibus reiciendis autem et incidunt laboriosam porro voluptate quaerat blanditiis beatae? Beatae optio quasi sed.
+      Necessitatibus officia aspernatur cumque sapiente atque id optio cupiditate repellat dolore neque. Mollitia sed sit recusandae ipsa distinctio id odit, doloribus saepe facilis sint. Vitae perferendis culpa officiis harum dicta.
+      Necessitatibus fuga est inventore. Ab nobis delectus est? Ab, unde, tenetur consequatur odio iure iusto error sapiente accusantium quidem cum ut modi ullam dolorum aperiam tempora voluptatem doloremque deserunt. Alias?
+      </p>
+    </div>
+    <div class="w50">
+      <img src="images/foto.jpg" alt="The company CEO at an rural environment">
+    </div>
+  </main><!-- main content -->
+
+  <section class="technologies">
+    <h3 class="title">Our main Technologies stack</h3>
+    <div class="center">
+      <div class="tech-box">
+        <h4>Icon1</h4>
+        <h4>Image for icon 1</h4>
+        <p>Text for icon 1</p>
+      </div>
+      <div class="tech-box">
+        <h4>Icon1</h4>
+        <h4>Image for icon 1</h4>
+        <p>Text for icon 1</p>
+      </div>
+      <div class="tech-box">
+        <h4>Icon1</h4>
+        <h4>Image for icon 1</h4>
+        <p>Text for icon 1</p>
+      </div>
+    </div>
+  </section><!-- technologies -->
+
+  <section class="testimonials">
+    <div class="center">
+      <div class="w50">
+        <h2 class="title">Testimonials</h2>
+        <div class="testimonial-text-box">
+          <p class="testimonial-content">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat dignissimos consequatur ea reprehenderit omnis sunt necessitatibus, sit sapiente. Enim aperiam porro aliquam corrupti eos cumque molestiae facere temporibus! Tempora, eum!</p>
+          <p class="testimonial-author">Lorem Ipsum</p>
+        </div><!-- testimonial text box -->
+        <div class="testimonial-text-box">
+          <p class="testimonial-content">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat dignissimos consequatur ea reprehenderit omnis sunt necessitatibus, sit sapiente. Enim aperiam porro aliquam corrupti eos cumque molestiae facere temporibus! Tempora, eum!</p>
+          <p class="testimonial-author">Lorem Ipsum</p>
+        </div><!-- testimonial text box -->
+        <div class="testimonial-text-box">
+          <p class="testimonial-content">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat dignissimos consequatur ea reprehenderit omnis sunt necessitatibus, sit sapiente. Enim aperiam porro aliquam corrupti eos cumque molestiae facere temporibus! Tempora, eum!</p>
+          <p class="testimonial-author">Lorem Ipsum</p>
+        </div><!-- testimonial text box -->
+      </div><!-- testimonials -->
+      <div class="w50">
+        <h2 class="title">Services</h2>
+        <ul class="services">
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque repellat quis necessitatibus perferendis, repellendus reprehenderit vitae! Vel, aspernatur fugiat. Illo hic eaque quia sit vitae alias reiciendis assumenda dolorum itaque.</li>
+          <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas nulla accusantium corrupti cumque, hic accusamus odit ut saepe deserunt. Labore ex a vel iusto tempora tempore velit necessitatibus eaque distinctio.</li>
+          <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis optio officia neque quos ipsum eos autem molestiae, officiis vel eius. Molestiae officiis neque aut recusandae tenetur quod, pariatur aperiam molestias?</li>
+        </ul><!-- services -->
+      </div><!-- service box -->
+    </div><!-- center -->
+  </section><!-- testimonials -->
+
+  <footer>
+    <div class="center">
+      <p>All rights reserved &copy; 2019</p>
+    </div>
+  </footer>
+
+</body>
+</html>
