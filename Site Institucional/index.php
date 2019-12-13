23ede8c3fc71c9da03c1b825dca5842355e92927
Paulo Lima <paulobruny@gmail.com>
2019-12-13 00:09:04 -0300

    Css para estilizar as secoes

--- Site Institucional/index.php
+++ Site Institucional/index.php
@@ -7,90 +7,106 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Welcome to BMD Company</title>
   <link rel="stylesheet" href="css/all.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
+
   <header>
-    <nav>
-      <ul class="desktop">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul> <!-- desktop -->
-      <ul class="mobile">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul><!-- mobile -->
-    </nav>
+    <div class="center">
+      <div class="logo left"><a href="#">BMD</a></div><!-- Logomarca -->
+      <nav class="desktop right">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav> <!-- desktop -->
+      <nav class="mobile right">
+        <div class="mobile-button"><i class="fas fa-bars"></i></div>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav> <!-- mobile -->
+      <div class="clear"></div>
+    </div><!-- center -->
   </header><!-- header -->
 
-  <section class="banner">
-    <form action="">
-      <h1>What is your main e-mail?</h1>
-      <input type="email" name="email" required>
-      <input type="submit" value="Register" name="register">
-    </form>
+  <section class="main-banner">
+    <div class="center">
+      <div class="overlay">
+        <form action="">
+          <h1>What is your main e-mail?</h1>
+          <input type="email" name="email" required>
+          <input type="submit" value="Register" name="register">
+        </form>
+      </div>
+    </div>
   </section><!-- banner -->
 
-  <main class="description">
-    <div class="w50">
-      <h2>BMG Company</h2>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium aut inventore officia consequatur deserunt, sit iusto adipisci reiciendis consequuntur mollitia voluptas laborum amet quae culpa iste tempora rerum et beatae.
-        Non unde eum odio saepe reprehenderit cumque quas corporis optio obcaecati incidunt. Explicabo quam dolor porro, dignissimos cum earum veritatis, ullam architecto expedita ratione eligendi? Perspiciatis ea ipsam dolorum sed!
-        Voluptas repudiandae facere saepe corrupti, dolorum dolores, quo nemo error vitae laudantium tempore totam eligendi necessitatibus reiciendis autem et incidunt laboriosam porro voluptate quaerat blanditiis beatae? Beatae optio quasi sed.
-        Necessitatibus officia aspernatur cumque sapiente atque id optio cupiditate repellat dolore neque. Mollitia sed sit recusandae ipsa distinctio id odit, doloribus saepe facilis sint. Vitae perferendis culpa officiis harum dicta.
-        Necessitatibus fuga est inventore. Ab nobis delectus est? Ab, unde, tenetur consequatur odio iure iusto error sapiente accusantium quidem cum ut modi ullam dolorum aperiam tempora voluptatem doloremque deserunt. Alias?
-      </p>
+  <section class="description">
+    <div class="center">
+      <div class="w50 left">
+        <h2 class="title">BMD Company</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium aut inventore officia consequatur deserunt, sit iusto adipisci reiciendis consequuntur mollitia voluptas laborum amet quae culpa iste tempora rerum et beatae.
+          Non unde eum odio saepe reprehenderit cumque quas corporis optio obcaecati incidunt. Explicabo quam dolor porro, dignissimos cum earum veritatis, ullam architecto expedita ratione eligendi? Perspiciatis ea ipsam dolorum sed!
+          Voluptas repudiandae facere saepe corrupti, dolorum dolores, quo nemo error vitae laudantium tempore totam eligendi necessitatibus reiciendis autem et incidunt laboriosam porro voluptate quaerat blanditiis beatae? Beatae optio quasi sed.
+          Necessitatibus officia aspernatur cumque sapiente atque id optio cupiditate repellat dolore neque. Mollitia sed sit recusandae ipsa distinctio id odit, doloribus saepe facilis sint. Vitae perferendis culpa officiis harum dicta.
+          Necessitatibus fuga est inventore. Ab nobis delectus est? Ab, unde, tenetur consequatur odio iure iusto error sapiente accusantium quidem cum ut modi ullam dolorum aperiam tempora voluptatem doloremque deserunt. Alias?
+        </p>
+      </div><!-- w50 -->
+      <div class="w50 left profile-image">
+        <img class="right" src="images/manager.jpg" alt="The company CEO at an rural environment">
+      </div><!-- w50 -->
+      <div class="clear"></div>
     </div>
-    <div class="w50">
-      <img src="images/foto.jpg" alt="The company CEO at an rural environment">
-    </div>
-  </main><!-- main content -->
+</section><!-- main content -->
 
   <section class="technologies">
-    <h3 class="title">Our main Technologies stack</h3>
     <div class="center">
-      <div class="tech-box">
-        <h4><i class="fab fa-html5"></i></h4>
-        <h4>Image for icon 1</h4>
-        <p>Text for icon 1</p>
+      <h3 class="title">Our main Technologies stack</h3>
+      <div class="tech-box w33 left">
+        <h3><i class="fab fa-html5"></i></h3>
+        <h4>HTML5</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui excepturi enim omnis adipisci nesciunt, consectetur deserunt quia, incidunt error provident harum sit quo sed quaerat. Nulla eveniet at amet provident!</p>
       </div>
-      <div class="tech-box">
-        <h4>Icon1</h4>
-        <h4>Image for icon 1</h4>
-        <p>Text for icon 1</p>
+      <div class="tech-box w33 left">
+        <h3><i class="fab fa-css3"></i></h3>
+        <h4>CSS3</h4>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque hic aut nostrum maxime molestias libero fugiat quis soluta deserunt? Magni atque pariatur doloremque inventore mollitia vitae nemo natus praesentium vel.</p>
       </div>
-      <div class="tech-box">
-        <h4>Icon1</h4>
-        <h4>Image for icon 1</h4>
-        <p>Text for icon 1</p>
+      <div class="tech-box w33 left">
+        <h3><i class="fab fa-js"></i></h3>
+        <h4>Javascript</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis culpa atque pariatur mollitia reprehenderit adipisci quae animi. Blanditiis ad vel alias provident tempore unde, eligendi neque quaerat iste veritatis amet.</p>
       </div>
     </div>
+    <div class="clear"></div>
   </section><!-- technologies -->
 
   <section class="testimonials">
     <div class="center">
-      <div class="w50">
+      <div class="w50 left">
         <h2 class="title">Testimonials</h2>
         <div class="testimonial-text-box">
           <p class="testimonial-content">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat dignissimos consequatur ea reprehenderit omnis sunt necessitatibus, sit sapiente. Enim aperiam porro aliquam corrupti eos cumque molestiae facere temporibus! Tempora, eum!</p>
-          <p class="testimonial-author">Lorem Ipsum</p>
+          <p class="testimonial-author">- John Doe</p>
         </div><!-- testimonial text box -->
         <div class="testimonial-text-box">
           <p class="testimonial-content">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat dignissimos consequatur ea reprehenderit omnis sunt necessitatibus, sit sapiente. Enim aperiam porro aliquam corrupti eos cumque molestiae facere temporibus! Tempora, eum!</p>
-          <p class="testimonial-author">Lorem Ipsum</p>
+          <p class="testimonial-author">- John Doe</p>
         </div><!-- testimonial text box -->
         <div class="testimonial-text-box">
           <p class="testimonial-content">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat dignissimos consequatur ea reprehenderit omnis sunt necessitatibus, sit sapiente. Enim aperiam porro aliquam corrupti eos cumque molestiae facere temporibus! Tempora, eum!</p>
-          <p class="testimonial-author">Lorem Ipsum</p>
+          <p class="testimonial-author">- John Doe</p>
         </div><!-- testimonial text box -->
       </div><!-- testimonials -->
-      <div class="w50">
+      <div class="w50 left">
         <h2 class="title">Services</h2>
         <ul class="services">
           <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque repellat quis necessitatibus perferendis, repellendus reprehenderit vitae! Vel, aspernatur fugiat. Illo hic eaque quia sit vitae alias reiciendis assumenda dolorum itaque.</li>
@@ -98,6 +114,7 @@
           <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis optio officia neque quos ipsum eos autem molestiae, officiis vel eius. Molestiae officiis neque aut recusandae tenetur quod, pariatur aperiam molestias?</li>
         </ul><!-- services -->
       </div><!-- service box -->
+      <div class="clear"></div>
     </div><!-- center -->
   </section><!-- testimonials -->
 
@@ -106,6 +123,7 @@
       <p>All rights reserved &copy; 2019</p>
     </div>
   </footer>
+
   <script src="js/all.min.js"></script>
 </body>
 
